be2110f0687adf69506551ea9c3bd52c6205817a
faishalam <faishalabdulmajid@gmail.com>
2024-03-10 21:24:10 +0700

    C-MotionNavbar-done

--- client/src/components/pages/homePage.jsx
+++ client/src/components/pages/homePage.jsx
@@ -1,7 +1,26 @@
-import Navbar from "../layouts/navbar"
+import { useState } from "react";
+import Navbar from "../layouts/navbar";
+
 
 export default function HomePage() {
+    const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+
     return (
-        <Navbar/>
-    )
+        <>
+            <Navbar isNavbarVisible={isNavbarVisible}/>
+            <div>
+                <div
+                    className="w-full h-12 absolute top-0 bottom-0 none" 
+                    onMouseEnter={() => setIsNavbarVisible(true)}
+                    onMouseLeave={() => setIsNavbarVisible(false)}>
+
+                </div>
+                <video src="https://assets.mixkit.co/videos/preview/mixkit-scary-woman-seeing-through-the-window-1987-large.mp4"
+                    autoPlay={true} loop muted
+                    className="w-full h-screen object-cover absolute top-0 bottom-0"
+                    style={{ zIndex: -1 }}
+                />
+            </div>
+        </>
+    );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,5 +8,5 @@ import router from './router'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
